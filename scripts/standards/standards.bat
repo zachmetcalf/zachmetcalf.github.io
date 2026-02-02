599fbfe8bef0defe7501bd9fe0c9511f998ae1c5
Zach Metcalf <zachmetcalf@gmail.com>
2026-02-01 21:35:53 -0800

    Update standards.bat

--- scripts/standards/standards.bat
+++ scripts/standards/standards.bat
@@ -15,44 +15,23 @@ mkdir %tempdir%
 
 pushd %tempdir%
 
-call :SetupGitRepo standards main https://github.com/zachmetcalf/zachmetcalf
+git clone --branch main https://github.com/zachmetcalf/zachmetcalf .
 
 popd
 
-copy /Y "%tempdir%\standards\.editorconfig"   "%projectdir%\.editorconfig"   >nul
-copy /Y "%tempdir%\standards\.gitattributes"  "%projectdir%\.gitattributes"  >nul
-copy /Y "%tempdir%\standards\.gitignore"      "%projectdir%\.gitignore"      >nul
-copy /Y "%tempdir%\standards\.clang-tidy"     "%projectdir%\.clang-tidy"     >nul
-
-if not exist "%projectdir%\.vscode" (
-	mkdir "%projectdir%\.vscode"
+if not exist %projectdir%\.vscode (
+	mkdir %projectdir%\.vscode
 )
 
-copy /Y "%tempdir%\standards\.vscode\settings.json" "%projectdir%\.vscode\settings.json" >nul
+copy /y %tempdir%\.vscode\settings.json %projectdir%\.vscode\settings.json >nul
+copy /y %tempdir%\.vscode\tasks.json %projectdir%\.vscode\tasks.json >nul
+copy /y %tempdir%\.clang-tidy %projectdir%\.clang-tidy >nul
+copy /y %tempdir%\.editorconfig %projectdir%\.editorconfig >nul
+copy /y %tempdir%\.gitattributes %projectdir%\.gitattributes >nul
+copy /y %tempdir%\.gitignore %projectdir%\.gitignore >nul
+copy /y %tempdir%\license.txt %projectdir%\license.txt >nul
 
-set error=%ERRORLEVEL%
-
-rmdir %tempdir% /s /q
-
-if not %error% == 0 (
-	echo standards failed
-	exit /b 1
-)
+rmdir /s /q %tempdir%
 
 echo standards succeeded
-exit /b 0
-
-:SetupGitRepo
-setlocal
-set repo=%~1
-set branch=%~2
-set url=%~3
-if not exist %repo% (
-    git clone --branch %branch% %url% %repo%
-) else (
-	pushd %repo%
-	git pull
-	popd
-)
-endlocal
 exit /b 0
